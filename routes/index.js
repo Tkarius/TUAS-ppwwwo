f07c0aa5fe65a87ee03d6910a0443c2cf1fdd668
Mira Pohjola <Mira.Pohjola89@gmail.com>
2018-11-24 13:46:37 +0200

    Minor changes made to pass needed extra parameters to render.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Comic Reviewer', pageTitle:'Home', pageDescription:'Welcome to Comic Reviewer!' });
 });
 
 module.exports = router;
