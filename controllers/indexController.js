542528d80ec5221bc8a11fb70f561bbc30981077
Mira Pohjola <Mira.Pohjola89@gmail.com>
2018-11-24 19:41:42 +0200

    Removed passing the title as render parameter as it is now defined in the layout.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,7 @@ exports.viewIndex = (req, res, next) => {
     //user defined here for layout testing purposes
     //in reality this is the user model of the current user
     let user = {
-        username:'blep'
+        username:undefined
     }
-    res.render('index', { title: 'Comic Reviewer', pageTitle:'Home', pageDescription:'Welcome to Comic Reviewer!', user:user});
+    res.render('index', {pageTitle:'Home', pageDescription:'Welcome to Comic Reviewer!', user:user});
 }
