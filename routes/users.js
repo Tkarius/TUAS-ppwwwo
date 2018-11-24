7d0c345f2a75bdb4e03e6e2ac63e6bb875f22f9a
Mira Pohjola <Mira.Pohjola89@gmail.com>
2018-11-24 14:45:46 +0200

    Added routes for listing users, viewing user details and registering (get + post). Currently missing requiring the user model.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var userController = require('../controllers/userController');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.users_list);
+
+/**GET user details */
+router.get('/:id', userController.user_details);
+
+router.get('/register', userController.user_create_get);
+
+router.post('/register', userController.user_create_post);
 
 module.exports = router;
