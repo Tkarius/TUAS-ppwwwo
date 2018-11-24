b5998e44e436d6be9e8c3359ee90636b58c2f038
Mira Pohjola <Mira.Pohjola89@gmail.com>
2018-11-24 20:31:37 +0200

    Skeleton controllers for comic list and details and author list and details. Made the 'Add Comic' link in layout functional.

--- controllers/comicController.js
+++ controllers/comicController.js
@@ -7,3 +7,30 @@ exports.comic_list = (req, res, next) => {
     }
     res.send('NOT IMPLEMENTED: comic_list');
 }
+
+exports.comic_details = (req, res, next) => {
+    //renders page for comic details
+    res.send('NOT IMPLEMENTED: comic_details');
+}
+
+exports.comic_add_get = (req, res, next) => {
+    //renders page for adding a new comic. 
+    //checks if user is logged in before rendering. If not, gives error
+    res.send('NOT IMPLEMENTED: comic_add_get');
+}
+
+exports.comic_add_post = (req, res, next) => {
+    //adds a new comic
+    //checks if user is logged in. If not, gives error
+    res.send('NOT IMPLEMENTED: comic_add_post');
+}
+
+exports.author_list = (req, res, next) => {
+    //renders page with list of authors
+    res.send('NOT IMPLEMENTED: author_list');
+}
+
+exports.author_details = (req, res, next) => {
+    //renders page with author details and listing of comics by the author
+    res.send('NOT IMPLEMENTED: author_details');
+}
