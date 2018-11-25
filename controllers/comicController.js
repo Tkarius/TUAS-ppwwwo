f47d0115aa8fd29df41a2556b3cccb3aa9b2372e
Mira Pohjola <Mira.Pohjola89@gmail.com>
2018-11-25 14:13:23 +0200

    Dummy version of comic listing which uses an example object instead of pulling real data from mongoDB. Calls rendering of the comic_listing template.

--- controllers/comicController.js
+++ controllers/comicController.js
@@ -3,8 +3,32 @@ exports.comic_list = (req, res, next) => {
     //if no sorting is requested, sort alphabetically
     let sorting = req.params.sorting;
     if (!sorting) {
-        sorting = 'alphabetical'
+        sorting = 'name'
     }
+
+    //placeholder object for testing
+    let comics = [
+        {
+            title: 'Testikomikki',
+            decription: 'testataan',
+            author: {
+                name: 'Esteri Testeri'
+            },
+            url: '/comics/comic/123',
+            source: 'http://www.google.com',
+        },
+        {
+            title: 'AlltheTesti',
+            decription: 'testataan lisää',
+            author: {
+                name: 'Antero Mertaranta'
+            },
+            url: '/comics/comic/1234',
+            source: 'http://www.google.com',
+        }
+    ]
+
+    res.render('comic_list', {pageTitle:'Browse Comics', pageDescription:'Browse added comics', comics:comics})
     res.send('NOT IMPLEMENTED: comic_list');
 }
 
